54e18a102c7f9e1638be66af600c9117c1b68a79
Allan Verde <allangreen2906@gmail.com>
2023-09-26 13:34:03 -0300

    feat: add sum function

---
+++ sum.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(Sum(2, 2))
+}
+
+func Sum(a, b int) int {
+	return a + b
+}
+
+func Sub(a, b int) int {
+	return a - b
+}
+
+func Mul(a, b int) int {
+	return a * b
+}
+
+func Div(a, b int) int {
+	return a / b
+}
---
+++ sum_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+
+	result := Sum(2, 3)
+
+	if result != 5 {
+		t.Error("The result must be 5")
+	}
+
+}
